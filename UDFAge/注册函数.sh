5db09ad7297347f97eb49f684c88a50ae3f25016
kian1990 <269590936@qq.com>
2024-06-24 10:15:03 +0800

    Add files via upload

---
+++ UDFAge/注册函数.sh
@@ -0,0 +1,6 @@
+# 临时注册，重启后失效
+hive> CREATE TEMPORARY FUNCTION age AS 'com.example.UDFAge' USING JAR '/root/UDFAge-1.0.jar';
+
+# 永久注册
+hadoop fs -put /root/UDFAge-1.0.jar /user/hive/warehouse
+hive> CREATE FUNCTION UDFAge AS 'com.example.UDFAge' USING JAR 'hdfs:///user/hive/warehouse/UDFAge-1.0.jar';
