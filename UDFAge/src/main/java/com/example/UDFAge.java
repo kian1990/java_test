91c2c9538e76f7907801c56f306133a803e230e2
kian1990 <269590936@qq.com>
2024-07-05 09:25:00 +0800

    Update UDFAge.java

--- UDFAge/src/main/java/com/example/UDFAge.java
+++ UDFAge/src/main/java/com/example/UDFAge.java
@@ -1,22 +1,17 @@
 package com.example;
 
-import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-@Description(name = "calculate_age",
-        value = "_FUNC_(idCard) - Calculate age from ID card",
-        extended = "Example:\n"
-                + "  SELECT calculate_age(idCard) FROM table_name"
-)
 public class UDFAge extends UDF {
 
     public int evaluate(String idCard) throws ParseException {
         int age = 0;
-        String birthdayStr = idCard.substring(6, 14); // 身份证号码中生日的字符串表示
+        String birthdayStr = idCard.substring(6, 14);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         Date birthday = sdf.parse(birthdayStr);
         Calendar birthdayCalendar = Calendar.getInstance();
@@ -26,7 +21,6 @@ public class UDFAge extends UDF {
 
         age = today.get(Calendar.YEAR) - birthdayCalendar.get(Calendar.YEAR);
 
-        // 如果今年的生日还没过，则年龄减一
         if (today.get(Calendar.MONTH) < birthdayCalendar.get(Calendar.MONTH)
                 || (today.get(Calendar.MONTH) == birthdayCalendar.get(Calendar.MONTH)
                 && today.get(Calendar.DAY_OF_MONTH) < birthdayCalendar.get(Calendar.DAY_OF_MONTH))) {
